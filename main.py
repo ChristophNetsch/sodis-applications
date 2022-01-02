b8b62002757a6d2e14cba8dc012758aff36ea4b3
Christoph Netsch <c.netsch@gmx.net>
2022-01-02 14:21:25 +0100

    poc

--- main.py
+++ main.py
@@ -1,3 +1,123 @@
+from typing import Tuple
 from dotenv import load_dotenv
+from time import sleep
+import openai
+import streamlit as st
+import os.path
+from pathlib import Path
+import json
+import logging
+from PIL import Image
 
 load_dotenv()
+KNOWLEDGE_FILEPATH = Path("wissensquellen")
+LOGO_IMG = Image.open('resources/nico_mario.png')
+FAVICON = Image.open('resources/favicon.png')
+JSONLNAME = "knowledge_jsonl.jsonl"  # Do not change
+
+def get_knowledge_file_options() -> Tuple[str]:
+    return tuple([*KNOWLEDGE_FILEPATH.glob("*.txt")])
+
+def convert_txt2jsonl(knowledge_jsonl:Path, knowledge_txt:Path) -> None:
+    # Process generating knew knowldege file
+    with open(knowledge_txt, encoding="utf8") as f:
+        lines = [{"text": line} for line in f.read().splitlines() if line]
+
+    # Convert to a list of JSON strings
+    json_lines = [json.dumps(l) for l in lines]
+
+    # Join lines and save to .jsonl file
+    json_data = '\n'.join(json_lines)
+    with open(knowledge_jsonl, 'w') as f:
+        f.write(json_data)
+
+def knowledge_available_online() -> bool:
+    return len(openai.File.list()['data']) >= 1
+
+# Process knowledge file
+def update_knowledge_base(knowledge_jsonl:Path, knowledge_available_online:Path):
+    # Delete existing file from API
+    if knowledge_available_online():
+        openai.File.delete(openai.File.list()['data'][0]['id'])
+        openai.File.list()
+
+    # Open knowledge base
+    openai.File.create(file=open(knowledge_jsonl, encoding="utf8"), purpose='answers')
+     
+def main() -> None:
+    # API call to openAI
+    logging.info(f"Connecting to OPENAI API via organization key {os.getenv('OPENAI_ORGANIZATION')}.")
+    openai.organization = os.getenv("OPENAI_ORG_ID") 
+    openai.api_key = os.getenv("OPENAI_SECRET")
+
+    st.set_page_config(page_title='Frag Nico-Mario', page_icon = FAVICON, layout = 'wide', initial_sidebar_state = 'auto')
+
+    st.image(LOGO_IMG.resize( [int(0.5 * s) for s in LOGO_IMG.size]), use_column_width=True)
+    st.header('Wähle eine Wissensquelle.')
+
+    knowledge_base_updated = False
+    kf_name = st.selectbox('Wähle eine hinterlegte Wissensquelle:', get_knowledge_file_options())
+    uploaded_file = st.file_uploader("Oder wähle eine eigene Wissensquelle (TXT-Datei):")
+
+    getfile = st.button("Einlesen..")
+
+    question = st.text_area('Was willst du wissen?')
+    button = st.button("Frag Nico-Mario!")
+
+    # Set filepaths for knowledge bank
+    knowledge_jsonl = Path(KNOWLEDGE_FILEPATH, JSONLNAME)
+    knowledge_txt = Path(KNOWLEDGE_FILEPATH, kf_name)
+
+    if uploaded_file:
+        string_data = uploaded_file.read()
+        lines = [{"text": line} for line in string_data.splitlines() if line]
+        # Convert to a list of JSON strings
+        json_lines = [json.dumps(l) for l in lines]
+
+        # Join lines and save to .jsonl file
+        json_data = '\n'.join(json_lines)
+        
+        with open(knowledge_jsonl, 'w') as f:
+            f.write(json_data)
+        
+        with st.spinner(text="Wissensquelle wird eingelesen..."):
+            update_knowledge_base(knowledge_jsonl, knowledge_available_online)
+        knowledge_base_updated = True
+
+    if getfile:
+        if not uploaded_file:
+            convert_txt2jsonl(knowledge_jsonl, knowledge_txt)
+        with st.spinner(text="Wissensquelle wird eingelesen..."):   
+            update_knowledge_base(knowledge_jsonl, knowledge_available_online)
+        knowledge_base_updated = True
+
+    # Knowledge engine question and answer
+    if button:
+        can_query = True
+        if not knowledge_base_updated:
+            if knowledge_jsonl.exists():
+                with st.spinner(text="Wissensquelle wird eingelesen..."):   
+                    update_knowledge_base(knowledge_jsonl, knowledge_available_online)
+                    sleep(5)
+            else:
+                st.error("Keine Wissensquelle online hinterlegt. Bitte wähle eine Wissensquelle aus oder lade eine eigene hoch.")
+                can_query = False    
+        
+        if can_query:
+            with st.spinner(text="Nico-Mario denkt nach..."):
+                response_json = openai.Answer.create(
+                    search_model="ada",
+                    model="davinci",
+                    question=question,
+                    file=openai.File.list()['data'][-1]['id'],  # Get latest file
+                    examples_context= "In 2017, U.S. life expectancy was 78.6 years." ,
+                    examples=[["What is human life expectancy in the United States?", "78 years."]],
+                    max_rerank=int(os.getenv("OPENAI_MAX_RERANK")),
+                    max_tokens=int(os.getenv("OPENAI_MAX_TOKENS")),
+                    stop=["\n", "<|endoftext|>"]
+                )
+            # Return answer to UI
+            st.markdown(response_json["answers"][0], unsafe_allow_html=False)
+            
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Wissensdatenbank
+
+impactu.txt
+
+"G:\Shared drives\03_Fundraising & Öffentlichkeitsarbeit - Aktion Sodis\06_Förderungen\ImpactU\02_Betreuung\2020_Pilotprojekt_Misereor\Solicitud Misereor - Desarrollo de la solicitud\Antrag\deutsch\20200716_aktueller Stand.docx"
+
+"G:\Shared drives\03_Fundraising & Öffentlichkeitsarbeit - Aktion Sodis\06_Förderungen\ImpactU\04_abgeschlossen\2021_Amprion\ImpacTú von Aktion Sodis e.V.docx"
+
+qhari_warmi.txt
+"G:\Shared drives\03_Fundraising & Öffentlichkeitsarbeit - Aktion Sodis\06_Förderungen\Qhari Warmi\01_Beantragung\2022_ALTERNAID\formlos\Antrag auf Finanzierung Projekt Kuska Purispa.docx"
+
+"G:\Shared drives\03_Fundraising & Öffentlichkeitsarbeit - Aktion Sodis\06_Förderungen\Qhari Warmi\01_Beantragung\2022_ALTERNAID\Antragsformular\ALTERNAID_Antrag.docx"
+
+ernaehrungssicherheit.txt
+"G:\Shared drives\03_Fundraising & Öffentlichkeitsarbeit - Aktion Sodis\06_Förderungen\Dorfschulen+Landwirt\01_Beantragung\Auslandsprojekte NRW 21\Projektantrag_0321_APNRW.pdf"
+"G:\Shared drives\03_Fundraising & Öffentlichkeitsarbeit - Aktion Sodis\06_Förderungen\Dorfschulen+Landwirt\01_Beantragung\Schmitz 2022\Teil_B_Projektantrag_Anm. Benedikt Schindele.docx"
+"G:\Shared drives\03_Fundraising & Öffentlichkeitsarbeit - Aktion Sodis\06_Förderungen\Dorfschulen+Landwirt\01_Beantragung\Rotary Mülheim an der Ruhr\Projektvorhaben_Aktion_Sodis_Ernährung_und_Gesundheit.docx"
